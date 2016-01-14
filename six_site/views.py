d5dd3604832e9cb28b4bed2366e83e76e4ab6166
loki <root@sixlab.cn>
2016-01-15 01:34:34 +0800

    so easy

--- six_site/views.py
+++ six_site/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'six_site/post_list.html', {'posts':posts})
